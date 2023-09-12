99fb3f49384c10fad85a8482378fc7ed9ef8451e
paragonyun <paragonyun@naver.com>
2023-09-12 09:37:50 +0900

    Main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from main_src.get_words import get_words
+from main_src.get_sentences import get_result_json
+
+"""
+main.py엔 아키텍처에 대한 설명이 들어갈 예정입니다!
+Will be Describe about the architecture of the project!
+"""
+
+if __name__ == "__main__":
+    get_words()
+    get_result_json()
